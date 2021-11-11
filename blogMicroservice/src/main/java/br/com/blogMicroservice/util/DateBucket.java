295884d0c738a8524590ad00e8a9815ccb902134
cesarboguchevski <cesar.boguchevski@gmail.com>
2021-11-11 01:43:51 -0300

    feat: include the bucket time algorithm and refactoring.

---
+++ blogMicroservice/src/main/java/br/com/blogMicroservice/util/DateBucket.java
@@ -0,0 +1,47 @@
+package br.com.blogMicroservice.util;
+
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+@lombok.Data
+public class DateBucket {
+    final Instant from;
+    final Instant to;
+
+    public static List<DateBucket> bucketize(
+            ZonedDateTime fromDate,
+            ZonedDateTime toDate,
+            int bucketSize,
+            ChronoUnit bucketSizeUnit
+    ) {
+        List<DateBucket> buckets = new ArrayList<>();
+
+        boolean reachedDate = false;
+
+        for (int i = 0; !reachedDate; i++) {
+            ZonedDateTime minDate = fromDate.plus(i * bucketSize, bucketSizeUnit);
+            ZonedDateTime maxDate = fromDate.plus((i + 1) * bucketSize, bucketSizeUnit);
+            reachedDate = toDate.isBefore(maxDate);
+            buckets.add(new DateBucket(minDate.toInstant(), maxDate.toInstant()));
+        }
+        return buckets;
+    }
+
+
+
+    public static List<DateBucket> bucketizeWithStream(ZonedDateTime fromDate, ZonedDateTime toDate,
+                                             int bucketSize, ChronoUnit bucketSizeUnit) {
+
+        return LongStream.range(0, bucketSizeUnit.between(fromDate, toDate)/bucketSize + 1)
+                .mapToObj( nb -> new DateBucket(
+                        fromDate.plus(nb * bucketSize, bucketSizeUnit).toInstant(),
+                        fromDate.plus((nb+1) * bucketSize, bucketSizeUnit).toInstant()))
+                .collect(Collectors.toList());
+    }
+}
--- blogMicroservice/src/test/java/br/com/blogMicroservice/BlogMicroserviceApplicationTests.java
+++ blogMicroservice/src/test/java/br/com/blogMicroservice/BlogMicroserviceApplicationTests.java
@@ -1,7 +1,18 @@
 package br.com.blogMicroservice;
 
+import br.com.blogMicroservice.util.DateBucket;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class BlogMicroserviceApplicationTests {
@@ -9,5 +20,37 @@ class BlogMicroserviceApplicationTests {
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	public void shouldBucketizeWithoutStream() {
+
+		// 'yyyy-MM-dd KK:mm:ss a' pattern
+		ZonedDateTime fromDate = ZonedDateTime.parse("2021-11-11 10:20:30 AM",
+				DateTimeFormatter.ofPattern("yyyy-MM-dd KK:mm:ss a").withZone(ZoneId.systemDefault()));
+
+		ZonedDateTime toDate = ZonedDateTime.parse("2021-11-11 10:20:35 AM",
+				DateTimeFormatter.ofPattern("yyyy-MM-dd KK:mm:ss a").withZone(ZoneId.systemDefault()));
+
+		List<DateBucket> bucketList = DateBucket.bucketize(fromDate, toDate, 1, ChronoUnit.SECONDS);
+
+		assertTrue(bucketList.isEmpty() ? false : true);
+	}
+
+	@Test
+	public void shouldBucketizeWithStream() {
+
+		// 'yyyy-MM-dd KK:mm:ss a' pattern
+		ZonedDateTime fromDate = ZonedDateTime.parse("2021-11-11 10:20:30 AM",
+				DateTimeFormatter.ofPattern("yyyy-MM-dd KK:mm:ss a").withZone(ZoneId.systemDefault()));
+
+		ZonedDateTime toDate = ZonedDateTime.parse("2021-11-11 10:20:35 AM",
+				DateTimeFormatter.ofPattern("yyyy-MM-dd KK:mm:ss a").withZone(ZoneId.systemDefault()));
+
+		List<DateBucket> bucketList = DateBucket.bucketizeWithStream(fromDate, toDate, 1, ChronoUnit.SECONDS);
+
+		assertTrue(bucketList.isEmpty() ? false : true);
+	}
+
+
 
 }
