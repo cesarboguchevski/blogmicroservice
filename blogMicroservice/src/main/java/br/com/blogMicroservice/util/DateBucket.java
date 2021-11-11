f2cdfbbb4747de1bd20df6265526552b8df386a9
cesarboguchevski <cesar.boguchevski@gmail.com>
2021-11-11 01:44:08 -0300

    feat: include the bucket time algorithm and refactoring.

--- blogMicroservice/src/main/java/br/com/blogMicroservice/util/DateBucket.java
+++ blogMicroservice/src/main/java/br/com/blogMicroservice/util/DateBucket.java
@@ -6,7 +6,6 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 @lombok.Data
